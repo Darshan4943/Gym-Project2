6603ae56a289b11d80697fc00718abfb381b1286
Darshan <darshanshinde4943@gmaul.com>
2023-05-05 00:55:45 +0530

    partial

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import styles from "./Footer.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+
+import styles from './Footer.module.css';
+
 export default function Footer() {
-  return (
-    <div className={styles.main}>
-        <h1>Footer</h1>
-    </div>
-  );
+    return (
+        <footer className={styles.footer}>
+        <h2>Social Follows</h2>
+  <div className={styles.social}>
+  <a href="https://www.youtube.com/c/jamesqquick"
+        className={styles.youtube }>
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+      <a href="https://www.facebook.com/learnbuildteach/"
+       className={styles.facebook }>
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://www.twitter.com/jamesqquick" className={styles.twitter }>
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a href="https://www.instagram.com/learnbuildteach"
+       className={styles.instagram }>
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+  </div>
+  <hr style={{borderTop:"1px solid black"}}/>
+  <p>&copy; Gold Gym</p>
+</footer>
+)
 }
